f3c096d9835bd0b91530e924f834f89db67f0c1b
Michael Mosmann <michael@mosmann.de>
2014-04-13 12:47:52 +0200

    game over works

--- crazyfrog/main.js
+++ crazyfrog/main.js
@@ -4,19 +4,19 @@ var game = new Phaser.Game(500, 600, Phaser.AUTO, 'game_div');
 var frog;
 var cars;
 var introText;
-var isGameOver=false;
+var isGameOver = false;
 
 // And now we define our first and only state, I'll call it 'main'. A state is a specific scene of a game like a menu, a game over screen, etc.
 var main_state = {
 
-    preload: function() {
-   		// Load a sprite in the game
-		// Parameters: name of the sprite, path to the image
-		game.load.image('car', 'assets/Car.png');  
-        game.load.image('frog', 'assets/frog2.png');  
+    preload: function () {
+        // Load a sprite in the game
+        // Parameters: name of the sprite, path to the image
+        game.load.image('car', 'assets/Car.png');
+        game.load.image('frog', 'assets/frog2.png');
     },
 
-    create: function() {
+    create: function () {
         game.physics.startSystem(Phaser.Physics.ARCADE);
         //game.physics.setBoundsToWorld();
 
@@ -26,24 +26,24 @@ var main_state = {
         frog.anchor.setTo(0.5, 0.5);
         frog.checkWorldBounds = true;
         game.physics.enable(frog, Phaser.Physics.ARCADE);
-        frog.body.checkCollision.any=true;
+        frog.body.checkCollision.any = true;
         frog.body.collideWorldBounds = true;
         frog.events.onOutOfBounds.add(frogOut, this);
 
         cars = game.add.group();
         cars.enableBody = true;
         cars.physicsBodyType = Phaser.Physics.ARCADE;
-    
+
         for (var i = 0; i < 4; i++) {
             var car_sprite = cars.create(0, 50 + i * 100, 'car');
-            car_sprite.body.velocity.x=50 + Math.random() * 200;
+            car_sprite.body.velocity.x = 50 + Math.random() * 200;
             car_sprite.checkWorldBounds = true;
             car_sprite.enableBody = true;
             car_sprite.physicsBodyType = Phaser.Physics.ARCADE;
             car_sprite.events.onOutOfBounds.add(carOut, this);
             game.physics.enable(car_sprite, Phaser.Physics.ARCADE);
-            car_sprite.body.checkCollision.any=true;
-            car_sprite.body.immovable=true;
+            car_sprite.body.checkCollision.any = true;
+            car_sprite.body.immovable = true;
         }
 
         introText = game.add.text(game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ffffff", align: "center" });
@@ -51,36 +51,37 @@ var main_state = {
         introText.visible = false;
     },
 
-    update: function() {
+    update: function () {
 
         game.physics.arcade.overlap(frog, cars, deadFrog, null, this);
 
-        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-            frog.y -= 5;
-            frog.angle = 0;
-        }
-        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-            frog.x -= 5;
-            frog.angle = 270;
-        }
-        if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-            frog.x += 5;
-            frog.angle = 90;
-        }
-        if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-            if (isGameOver) {
-                isGameOver=false;
-                introText.visible=false;
-
-                frog.x=game.width / 2;
-                frog.y=500;
+        if (!isGameOver) {
+            if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+                frog.y -= 5;
                 frog.angle = 0;
-                frog.body.velocity.setTo(0,0);
+            }
+            if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                frog.x -= 5;
+                frog.angle = 270;
+            }
+            if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+                frog.x += 5;
+                frog.angle = 90;
+            }
+        } else {
+            if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+                isGameOver = false;
+                introText.visible = false;
+
+                frog.x = game.width / 2;
+                frog.y = 500;
+                frog.angle = 0;
+                frog.body.velocity.setTo(0, 0);
             }
         }
-	},
+    },
 
-    render: function() {
+    render: function () {
         game.debug.body(frog);
         game.debug.spriteInfo(frog);
     }
@@ -94,21 +95,21 @@ function carOut(car) {
 }
 
 function frogOut(lfrog) {
-    lfrog.body.velocity.setTo(0,0);
-    lfrog.x=game.width / 2;
-    lfrog.y=500;
+    lfrog.body.velocity.setTo(0, 0);
+    lfrog.x = game.width / 2;
+    lfrog.y = 500;
     lfrog.angle = 0;
-    isGameOver=true;
+    isGameOver = true;
 }
 
 function deadFrog(frog, car) {
     introText.text = 'Game Over!';
     introText.visible = true;
-    isGameOver=true;
+    isGameOver = true;
 
 }
 
 
 // And finally we tell Phaser to add and start our 'main' state
-game.state.add('main', main_state);  
+game.state.add('main', main_state);
 game.state.start('main');  
